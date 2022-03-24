c1553ce4fc21cc2a0acf98967899d4f14e42356c
annngure <annwanjikungure@gmail.com>
2022-03-25 00:41:44 +0300

    deploy

--- tourapp/tests.py
+++ tourapp/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
-
+from .models import *
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+
+    def setUp(self):
+        self.ann = profile(name="Try",Location="Townie",occupation="Driver")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.ann,profile))
+
+    def tearDown(self):
+        profile.objects.all().delete()
+
+    def test_save_method(self):
+        self.ann.save_profile()
+        profile= Profile.objects.all()
+        self.assertTrue(len(profile)>0)
+
+    def test_delete_method(self):
+        self.ann.delete_profile('Site')
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)==0)
+
+class LocalsTestClass(TestCase):
+    def setUp(self):
+        self.ann = locals(name="Try",Location="Townie",email="services@gmail.com")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.ann,locals))
+
+    def tearDown(self):
+        locals.objects.all().delete()
+
+    def test_save_method(self):
+        self.ann.save_locals()
+        locals= Locals.objects.all()
+        self.assertTrue(len(locals)>0)
+
+    def test_delete_method(self):
+        self.ann.delete_locals('Site')
+        locals = Locals.objects.all()
+        self.assertTrue(len(locals)==0)
+        
--- tourism/settings.py
+++ tourism/settings.py
@@ -13,8 +13,13 @@ from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import os
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
+import sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -51,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'tourism.urls'
@@ -77,12 +83,34 @@ WSGI_APPLICATION = 'tourism.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+# development
+if config('MODE')=="dev":
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
+       
+   }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv()
 
 
 # Password validation
@@ -126,6 +154,26 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# configuring the location for media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
 
 #Adding config
 
